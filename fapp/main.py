2b59287eb7d5a1e52d5e2718a57aaf8a4ff8ebde
21size <askelpyrip@yandex.ru>
2024-12-02 19:16:33 +0300

    docker

--- fapp/main.py
+++ fapp/main.py
@@ -15,18 +15,20 @@ async def root():
           <link rel="stylesheet" href="style.css" />
         </head>
         <body>
-         window.YaAuthSuggest.init(
-    {
-      client_id: "c865ee8244ea45b6b97d96421399ae1f",
-      response_type: "token",
-      redirect_uri: "https://cosm-report.podruge-apps.ru/"
-    },
-    "https://cosm-report.podruge-apps.ru",
-    { view: "default" }
-  )
-  .then(({handler}) => handler())
-  .then(data => console.log('Сообщение с токеном', data))
-  .catch(error => console.log('Обработка ошибки', error))
+         <script>
+            window.YaAuthSuggest.init(
+                {
+                  client_id: "c865ee8244ea45b6b97d96421399ae1f",
+                  response_type: "token",
+                  redirect_uri: "https://cosm-report.podruge-apps.ru/"
+                },
+                "https://cosm-report.podruge-apps.ru",
+                { view: "default" }
+                )
+              .then(({handler}) => handler())
+              .then(data => console.log('Сообщение с токеном', data))
+              .catch(error => console.log('Обработка ошибки', error))
+         </script>
         </body>
         </html>
         """
